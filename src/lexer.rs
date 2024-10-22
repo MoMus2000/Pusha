c354dc75074e96c3d8e682e3279120cb5dc54fcb
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-10-21 20:44:52 -0400

    Fix build system and make it usable

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,18 +7,18 @@ pub struct Lexer {
 impl Lexer {
     pub fn lex(&self) -> Vec<OpCode> {
         let mut tokens = Vec::new();
-        let source_code = self.source_code.split(" ");
-        for token in source_code {
-            match token {
-                "+" => tokens.push(OpCode::OpAdd),
-                "-" => tokens.push(OpCode::OpSub),
-                "." => tokens.push(OpCode::OpDump),
-                _ => {
-                    if let Ok(number) = token.parse::<i64>() {
-                        tokens.push(OpCode::OpPush(number));
-                    } else {
-                        println!("Not handled: {}", token);
-                        todo!()
+        let source_code = self.source_code.split("\n");
+        for line in source_code {
+            for token in line.split(" ") {
+                match token {
+                    "+" => tokens.push(OpCode::OpAdd),
+                    "-" => tokens.push(OpCode::OpSub),
+                    "." => tokens.push(OpCode::OpDump),
+                    _ => {
+                        if let Ok(number) = token.parse::<i64>() {
+                            tokens.push(OpCode::OpPush(number));
+                        } else {
+                        }
                     }
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod lexer;
-use std::{error::Error, fs, io::Read};
 
 use crate::lexer::Lexer;
+use std::{
+    env,
+    error::Error,
+    fs,
+    io::{Read, Write},
+    process::exit,
+};
 
 pub enum OpCode {
     OpPush(i64),
@@ -11,14 +17,40 @@ pub enum OpCode {
 }
 
 pub struct Program {
-    source_code: Vec<OpCode>,
+    tokens: Vec<OpCode>,
 }
 
 impl Program {
-    fn run_compilation(&self) {}
+    pub fn new(source_code_path: String) -> Result<Program, Box<dyn Error>> {
+        let mut source_code = String::new();
+        fs::File::open(source_code_path)?.read_to_string(&mut source_code)?;
+        let lexer = Lexer { source_code };
+        let tokens = lexer.lex();
+        Ok(Program { tokens })
+    }
+
+    fn run_compilation(&self) -> Result<(), Box<dyn Error>> {
+        let mut temp_file = tempfile::tempfile()?;
+        temp_file.write(b"section .text\n")?;
+        temp_file.write(b"  global _start")?;
+        temp_file.write(b"_start:\n")?;
+        temp_file.write(b"mov rax, 60\n")?;
+        temp_file.write(b"xor rdi, rdi\n")?;
+        temp_file.write(b"syscall\n")?;
+        for token in &self.tokens {
+            match token {
+                OpCode::OpAdd => {}
+                OpCode::OpPush(i) => {}
+                OpCode::OpSub => {}
+                OpCode::OpDump => {}
+            }
+        }
+        Ok(())
+    }
+
     fn run_simulation(&self) -> Result<(), Box<dyn Error>> {
         let mut stack: Vec<i64> = vec![];
-        for token in &self.source_code {
+        for token in &self.tokens {
             match token {
                 OpCode::OpAdd => {
                     let a = stack.pop().unwrap();
@@ -41,15 +73,34 @@ impl Program {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut source_code = String::new();
-    fs::File::open("./examples/test.psh")?.read_to_string(&mut source_code)?;
-    let lexer = Lexer { source_code };
-    let tokens = lexer.lex();
+fn usage(program_name: &str) {
+    println!("Usage {} <SUBCOMMAND> [ARGS]", program_name);
+    println!("SUBCOMMANDS:");
+    println!("  sim <file> Simulate the program.");
+    println!("  com <file> Compile the program.");
+}
 
-    let program = Program {
-        source_code: tokens,
-    };
-    program.run_simulation();
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let program_name = &args[0];
+    if args.len() < 3 {
+        usage(&program_name);
+        exit(1)
+    }
+    let command = args[1].as_str();
+    let source_path = args[2].as_str();
+    let program = Program::new(source_path.to_string())?;
+    match command {
+        "sim" => {
+            program.run_simulation()?;
+        }
+        "com" => {
+            program.run_compilation()?;
+        }
+        _ => {
+            usage(program_name);
+            exit(1)
+        }
+    }
     Ok(())
 }
